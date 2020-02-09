2914448ae67461d80cc215c8c8a47139984b5585
Adrian Pilarczyk <adrian@useo.pl>
2020-02-09 17:33:59 +0100

    Change sitter model to mongo model

--- src/sitters/sitters.model.ts
+++ src/sitters/sitters.model.ts
@@ -1,6 +1,8 @@
-export interface Sitter {
-  id: string;
-  first_name: string;
-  last_name: string;
-  gender: string;
+import { Document } from 'mongoose';
+
+export interface Sitter extends Document {
+  readonly id: string;
+  readonly first_name: string;
+  readonly last_name: string;
+  readonly gender: string;
 }
